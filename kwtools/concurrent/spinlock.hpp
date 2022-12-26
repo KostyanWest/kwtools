3f6c8566e944305849fbd6325d8c3521f9d39f62
Kostyan West <sonkosdim@tut.by>
2022-12-26 18:30:15 +0300

    Добавлены flags.hpp и spinlock.hpp

---
+++ kwtools/concurrent/spinlock.hpp
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2022 KostyanWest
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See LICENSE.txt or http://www.boost.org/LICENSE_1_0.txt)
+
+// An idea from https://github.com/microsoft/STL/issues/680#issuecomment-669349811
+
+#ifndef KWTOOLS_CONCURRENT_SPINLOCK_HPP
+#define KWTOOLS_CONCURRENT_SPINLOCK_HPP
+
+#include <kwtools/stuff.hpp>
+
+#include <emmintrin.h>
+
+
+namespace kwt::concurrent
+{
+
+
+template<typename ExchangeFunc, typename CheckFunc>
+void spin_until( ExchangeFunc exchange, CheckFunc check )
+{
+	num mask = 1;
+	const num max = 64;
+	while (exchange())
+	{
+		while (check())
+		{
+			for (int i = mask; i; --i)
+			{
+				_mm_pause();
+			}
+			mask = mask < max ? mask << 1 : max;
+		}
+	}
+}
+
+
+} // namespace kwt::concurrent
+
+#endif // !KWTOOLS_CONCURRENT_SPINLOCK_HPP
---
+++ kwtools/flags.hpp
@@ -0,0 +1,69 @@
+﻿// Copyright (c) 2022 KostyanWest
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See LICENSE.txt or http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef KWTOOLS_FLAGS_HPP
+#define KWTOOLS_FLAGS_HPP
+
+#include <type_traits>
+
+
+
+namespace kwt
+{
+
+
+template <typename Enum>
+class flags
+{
+public:
+	using enum_type = std::underlying_type_t<Enum>;
+
+	constexpr flags() noexcept : underlying() {}
+	constexpr flags( const Enum e ) noexcept : underlying( static_cast<uint16_t>(e) ) {}
+	constexpr explicit flags( const enum_type v ) noexcept : underlying( v ) {}
+
+	constexpr explicit operator enum_type() const noexcept 
+	{
+		return underlying;
+	}
+	constexpr explicit operator bool() const noexcept
+	{
+		return bool( underlying );
+	}
+
+	constexpr bool operator==( const flags& other ) const
+	{
+		return underlying == other.underlying;
+	}
+	constexpr bool operator!=( const flags& other ) const
+	{
+		return underlying != other.underlying;
+	}
+
+	constexpr flags operator&( const flags& other ) const
+	{
+		return flags( underlying & other.underlying );
+	}
+	constexpr flags operator|( const flags& other ) const
+	{
+		return flags( underlying | other.underlying );
+	}
+	constexpr flags operator^( const flags& other ) const
+	{
+		return flags( underlying ^ other.underlying );
+	}
+	constexpr flags operator~( const flags& other ) const
+	{
+		return flags( underlying ~ other.underlying );
+	}
+
+private:
+	enum_type underlying;
+};
+
+
+} // namespace kwt
+
+#endif // !KWTOOLS_FLAGS_HPP
